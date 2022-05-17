dff94d05a7d59ca0e9ebe29ec1ec6dea2e04d939
IsaiasNascimentoSilva <isaias.silva@ccc.ufcg.edu.br>
2022-05-17 17:31:56 -0300

    Criado Exibição de clientes e o controler

---
+++ controller.py
@@ -0,0 +1,4 @@
+'''
+    *Responsável por controlar todo sistema, lidar com banco de dados e entidades
+    *@uthor: Isaías Silva
+'''
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tkinter.ttk import*
 from tkinter import*
 
-from numpy import record
 from produto import Produto
 
 # Atributos------------------------------------
@@ -12,7 +11,7 @@ root = Tk()
 root.title('Gerency')
 root.geometry('1400x700')
 
-# Topo
+#------------------------Topo------------------------------
 f_top = Frame(root,width=700,height=60,background='#32cd32')
 f_top.grid(row=0,column=0)
 
@@ -47,13 +46,13 @@ ajudaB.place(x=280,y=10)
 c_categ = Combobox(root,justify='center')
 c_categ['values'] = ('<CATEGORIA>','Suco','Drink','Cerveja','Dose','Petisco','Refeição','Sanduíche','Diversos')
 c_categ.current(0)
-c_categ.grid(row=1,column=0)
+c_categ.grid(column=0,row=1)
 
-# frame de Produtos
+#-----------------------------------frame de Produtos----------------------------------------
 f_prod = Frame(root,width=700,height=240,background='#dcdcdc')
 f_prod.grid(row=2,column=0)
 
-# Exibi os Produtos-------------------------------------------
+#....................................Exibi os Produtos........................................
 # Definir as colunas
 col = ('nome','codigo','valor')
 tree = Treeview(f_prod,columns=col,show='headings')
@@ -78,5 +77,41 @@ tree.grid(row=0,column=0,sticky='nsew')
 scrollbar = Scrollbar(f_prod,orient=VERTICAL,command=tree.yview)
 tree.configure(yscroll=scrollbar.set)
 scrollbar.grid(row=0,column=1,sticky='ns')
+
+#--------------------------------frame de clientes------------------------------------------
+f_client = Frame(root,width=700,height=320,bg='#dcdcdc')
+f_client.grid(row=3,column=0)
+
+# Botões 
+b_exebir = Button(f_client,text='Exibir Pendências',font='Arial 12')
+b_exebir.place(x=40, y=20)
+
+b_ocutar = Button(f_client,text='Ocultar Pendências',font='Arial 12')
+b_ocutar.place(x=220, y=20)
+
+#....................................Tabela de pendências........................................
+# Definir as colunas
+col = ('nome','cpf')
+pedence = Treeview(f_client,columns=col,show='headings')
+
+# Define os titulos
+pedence.heading('nome',text='Nome')
+pedence.heading('cpf',text='Cpf')
+
+# Inserir dados na treeview
+
+# função de evento 
+def selected1(event):
+    for item_select in pedence.selection():
+        item = pedence.item(item_select)
+        selec = item['values']
+        
+pedence.bind('<<TreeviewSelet>>', selected1)
+pedence.place(x=40,y=80)
+
+# Add deslizadores auxiliar
+scrollbar1 = Scrollbar(f_client,orient=VERTICAL,command=pedence.yview)
+pedence.configure(yscroll=scrollbar1.set)
+scrollbar1.place(x=440,y=80)
 
 root.mainloop()
